086f8bc998b07f443e9316c02fa64255cd1d39fb
Jordan Spencer <Jordan.S.Spencer@gmail.com>
2024-11-26 07:49:07 -0500

    update

---
+++ leetcode/68-text-justification.js
@@ -0,0 +1,34 @@
+/**
+ * @param {string[]} words
+ * @param {number} maxWidth
+ * @return {string[]}
+ */
+var fullJustify = function (words, maxWidth) {};
+
+const words1 = ["This", "is", "an", "example", "of", "text", "justification."];
+const maxWidth1 = 16;
+const words2 = ["What", "must", "be", "acknowledgment", "shall", "be"];
+const maxWidth2 = 16;
+const words3 = [
+  "Science",
+  "is",
+  "what",
+  "we",
+  "understand",
+  "well",
+  "enough",
+  "to",
+  "explain",
+  "to",
+  "a",
+  "computer.",
+  "Art",
+  "is",
+  "everything",
+  "else",
+  "we",
+  "do",
+];
+const maxWidth3 = 20;
+
+console.log(fullJustify());
